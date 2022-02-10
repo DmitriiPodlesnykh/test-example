6deb1b20a3417c3054035d2d517bace06067641c
Dmitrii Podlesnykh <dmitrii.podlesnykh@gmail.com>
2022-02-10 12:10:29 +0300

    Add ImportantLogic.java

--- src/test/java/com/podlesnykh/demo/testexample/calculator/CalculatorServiceTest.java
+++ src/test/java/com/podlesnykh/demo/testexample/calculator/CalculatorServiceTest.java
@@ -21,7 +21,7 @@ class CalculatorServiceTest {
     void testCalculate_1plus1_return2() {
         double result = sut.calculate(1, 1, "+");
 
-        Assertions.assertEquals(2, result);
+        Assertions.assertEquals(5, result);
     }
 
     @Test
